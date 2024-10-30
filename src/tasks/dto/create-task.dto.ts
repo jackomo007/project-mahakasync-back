9057f2b75d5e5d129cd24b20e7c36c92ac8d577a
angel <jeal.code47@gmail.com>
2024-10-30 13:38:16 -0300

    feat: implement swagger

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   IsString,
   IsOptional,
@@ -7,21 +8,26 @@ import {
 } from "class-validator";
 
 export class CreateTaskDto {
+  @ApiProperty({ description: "The title of the task" })
   @IsString()
   readonly title: string;
 
+  @ApiProperty({ description: "The description of the task", required: false })
   @IsOptional()
   @IsString()
   readonly description?: string;
 
+  @ApiProperty({ description: "The due date of the task", required: false })
   @IsOptional()
   @IsDateString()
   readonly dueDate?: string;
 
+  @ApiProperty({ description: "The status of the task", required: false })
   @IsOptional()
   @IsEnum(["pending", "in-progress", "completed"])
   readonly status?: string;
 
+  @ApiProperty({ description: "The priority of the task", required: false })
   @IsOptional()
   @IsEnum(["low", "medium", "high"])
   readonly priority?: string;
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   IsString,
   IsOptional,
@@ -7,22 +8,27 @@ import {
 } from "class-validator";
 
 export class UpdateTaskDto {
+  @ApiProperty({ description: "The title of the task", required: false })
   @IsOptional()
   @IsString()
   readonly title?: string;
 
+  @ApiProperty({ description: "The description of the task", required: false })
   @IsOptional()
   @IsString()
   readonly description?: string;
 
+  @ApiProperty({ description: "The due date of the task", required: false })
   @IsOptional()
   @IsDate()
   readonly dueDate?: Date;
 
+  @ApiProperty({ description: "The status of the task", required: false })
   @IsOptional()
   @IsEnum(["pending", "in-progress", "completed"])
   readonly status?: string;
 
+  @ApiProperty({ description: "The priority of the task", required: false })
   @IsOptional()
   @IsEnum(["low", "medium", "high"])
   readonly priority?: string;
